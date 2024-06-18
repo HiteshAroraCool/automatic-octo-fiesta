6917b25c9d284f70e956e46ab2add67528dc5091
HiteshAroraCool <76745725+HiteshAroraCool@users.noreply.github.com>
2024-06-19 01:29:00 +0530

    Update app.py
    it works

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from selenium import webdriver
 import os
-from selenium.webdriver.chrome.options import Options
 
-os.environ["PATH"] += r":/usr/local/SeleniumDrivers"
+os.environ["PATH"] += r"C:/SeleniumDrivers"
 print(os.environ["PATH"])
 
-
-options = ChromeOptions()
-options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 driver.get('http://selenium.dev')
-driver.quit()
