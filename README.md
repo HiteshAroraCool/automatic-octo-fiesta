eafc49285563676cf908e11dc850bc0d5c9f55d4
Hitesh Arora <76745725+HiteshAroraCool@users.noreply.github.com>
2024-06-18 19:31:42 +0000

    ok

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # automatic-octo-fiesta
+
+I used codespace: build new dev config to genrate .devcontainer floder, this pre install python, postgre and lib in requirements.txt file on codespace upon start.
+
+using selenium, a web automation tool, I will scrape the require information.
--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from selenium import webdriver
+import os
+from selenium.webdriver.chrome.options import Options
+
+os.environ["PATH"] += r":/usr/local/SeleniumDrivers"
+print(os.environ["PATH"])
+
+
+options = ChromeOptions()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
+driver.get('http://selenium.dev')
+driver.quit()
