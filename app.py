7dabed0d22310fe99b30a39606762793ffd9a051
HiteshAroraCool <76745725+HiteshAroraCool@users.noreply.github.com>
2024-06-20 22:52:43 +0530

    Links Extracted

--- app.py
+++ app.py
@@ -15,40 +15,44 @@ print(os.environ["PATH"])
 driver = webdriver.Chrome()
 driver.get('https://www.google.com/')
 
-time.sleep(2)
+time.sleep(1)
 
 # Google Search for "arcgis/rest/services"
 search_bar = driver.find_element(By.CSS_SELECTOR, '[name="q"]')
 search_bar.send_keys("arcgis/rest/services")
 search_bar.send_keys(Keys.RETURN)
 
-time.sleep(2)
+time.sleep(1)
 
 URL_links = []
-while len(URL_links) <= 100:
-    
-    # Getting URL_links
-    # <cite class="qLRx3b tjvcx GvPZzd cHaqb" role="text">https://developers.arcgis.com<span class="ylgVCe ob9lvb" role="text"> › rest › services-reference</span></cite>
-    cite_elements = driver.find_elements(By.TAG_NAME, "cite")
 
-    # Extract and print the URLs
-    for element in cite_elements:
-        if "arcgis" and "rest" and "services" in element.text:
-            URL_links.append(element.text)
-
-    driver.find_element(by=By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
-    time.sleep(1)
+while True:
     try:
         #<a class="T7sFge sW9g3e VknLRd __web-inspector-hide-shortcut__" href="/search?q=google&amp;sca_esv=453f36eb1da0db3c&amp;sca_upv=1&amp;sxsrf=ADLYWILpCMZphdwloOSongsix67lh9jpHw:1718887947976&amp;ei=CyZ0ZrCBO9jZseMP3tWk0Ag&amp;start=10&amp;sa=N" style="transform: scale(1);" jsname="oHxHid" jsaction="qBEZuc" aria-label="More results" data-ve-view="" role="button" data-hveid="CAkQBA" data-ved="2ahUKEwjwsrDMnOqGAxXYbGwGHd4qCYoQqq4CegQICRAE" aria-hidden="false"><hr class="KXbwLb" aria-hidden="true"><h3 aria-hidden="true"><div class="GNJvt ipz2Oe"><span class="kQdGHd"><span class="OTvAmd z1asCe QFl0Ff"><svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path></svg></span></span><span class="RVQdVd">More results</span></div></h3></a>
         # https://www.google.com/search?q=arcgis/rest/services&sca_esv=453f36eb1da0db3c&sca_upv=1&source=hp&ei=9Sl0ZsXsNoTX1e8PwtyZ8AM&iflsig=AL9hbdgAAAAAZnQ4BcOeJKbZ3lqqsvA6Y5TnrBOluZWK&uact=5&oq=arcgis/rest/services&gs_lp=Egdnd3Mtd2l6IhRhcmNnaXMvcmVzdC9zZXJ2aWNlc0hYUABYM3AAeACQAQCYAQCgAQCqAQC4AQPIAQD4AQGYAgCgAgCYAwCSBwCgBwA&sclient=gws-wiz&aomd=1
-        
         # Locate the "More results" link element by text
         driver.find_element(By.XPATH, '//a[@aria-label="More results"]').click()
         print("More result")
-
     except:
+        print("Page Down")
         driver.find_element(by=By.TAG_NAME, value="body").send_keys(Keys.PAGE_DOWN)
         time.sleep(1)
+    try:
+        i_element_xpath = '//i[contains(text(), "In order to show you the most relevant results, we have omitted some entries very similar to the 160 already displayed.")]'
+        if driver.find_element(By.XPATH, i_element_xpath):
+            print("Exiting")
+            break
+    except:
+        continue
+
+# Getting URL_links
+# <cite class="qLRx3b tjvcx GvPZzd cHaqb" role="text">https://developers.arcgis.com<span class="ylgVCe ob9lvb" role="text"> › rest › services-reference</span></cite>
+cite_elements = driver.find_elements(By.TAG_NAME, "cite")
+
+# Extract and print the URLs
+for element in cite_elements:
+    if "arcgis" and "rest" and "services" in element.text:
+        URL_links.append(element.text)
 driver.quit()
 
 # Initialize the dictionary with empty lists for columns
